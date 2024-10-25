c2df58e524d6ee259f2566160230118603579ab6
Meghana629 <meghanakatkam01@gmail.com>
2024-10-25 21:08:21 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,47 @@
+// switch statements
+switch("friday")
+{
+    case "monday":
+         console.log("today is monday")
+    break;
+
+    case "tuesday":
+    console.log("today is tuesday ")
+    break;
+
+    case "wednesday":
+    console.log("today is wednesday")
+    break;
+
+    case "thursday":
+    console.log("today is thursday")
+    break;
+
+    case "friday":
+    console.log("today is friday")
+    break;
+
+    case "saturday":
+    console.log("today is saturday")
+    break;
+
+    case "sunday":
+    console.log("today is sunday")
+    break;
+
+    default:("there is no day")
+}
+
+
+// for...of
+var a=[1,2,3,4,5];
+for(var i=1;i<=a.length;a++){
+    console.log(a)
+}
+
+// for...of
+
+var a="meghana";
+for(var char of a){
+    console.log(char)
+}
